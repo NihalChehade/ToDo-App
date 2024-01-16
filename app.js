0d762ea3b5ea33a29d1c143bea2d4cc7808f5019
Nihal Chehade <nihalche@icloud.com>
2024-01-15 23:05:48 -0800

    final commit

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
 const form = document.querySelector("form");
 const input = document.querySelector("#newToDoItem");
-const ul = document.querySelector("#TODOs");
+const ol = document.querySelector("#TODOs");
 
 form.addEventListener("click", function (ev) {
   ev.preventDefault();
   if (input.value && ev.target.tagName === "BUTTON") {
-    ul.prepend(addNewTODO(input.value));
+    newLi = addNewTODO(input.value);
+    ol.prepend(newLi);
     input.value = "";
   }
 });
 
 function addNewTODO(txt) {
-  const TODOLi = document.createElement("LI");
+  const TODOLi = document.createElement("li");
   const newCheckBox = document.createElement(`input`);
   const newBtn = document.createElement("button");
   newCheckBox.setAttribute("type", "checkbox");
+  newBtn.setAttribute("class", "delete");
+  newBtn.innerText = "X";
   TODOLi.innerText = txt;
-  TODOLi.prepend(newCheckBox);
+  TODOLi.append(newCheckBox);
   TODOLi.append(newBtn);
   return TODOLi;
 }
 
+ol.addEventListener("click", function (e) {
+  if (e.target.checked) {
+    e.target.parentElement.classList.add("strikethrough");
+  } else {
+    e.target.parentElement.classList.remove("strikethrough");
+  }
 
-ul.addEventListener("click", function(e){
-    if(e.target.checked){
-        e.target.parentElement.classList.add("strikethrough");
-    } else {
-        e.target.parentElement.classList.remove("strikethrough");
-    }
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+    // const savedArray=JSON.parse(localStorage.liArray);
+    // savedArray.
+  }
+});
 
-    if(e.target.tagName==="BUTTON"){
-        e.target.parentElement.remove();
+window.addEventListener("unload", function () {
+  if (!ol.children) {
+    localStorage.clear();
+    
+  } else {
+    localStorage.setItem("olLi", ol.innerHTML);
+  }
+});
+window.addEventListener("load", function () {
+  
+    const lis = localStorage.getItem("olLi");
+    if(lis){
+    ol.innerHTML=lis;
+    const liElements=ol.querySelectorAll("LI");
+    
+    for(let li of liElements){
+      if(li.className==="strikethrough"){
+        const checkboxInsideLi=li.querySelector(`input[type="checkbox"]`);
+      checkboxInsideLi.checked=true;
+      }
+
     }
+  }
 });
